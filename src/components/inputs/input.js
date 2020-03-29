5ef1e9d9cd5e690b6509e2b6c8418780b96d8356
Roman Juchimcuk <romasjuch@gmail.com>
2020-03-29 22:16:04 +0300

    add function to get value from input field

--- src/components/inputs/input.js
+++ src/components/inputs/input.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 export default class Input extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (e) => {
+        this.props.changeHandler(e.tartget.value)
+    }
+
     render(){
         return(
             <div className='input'>
-                <input className='input__field' type={this.props.type} placeholder={this.props.placeholder} />
+                <input 
+                    className='input__field' 
+                    type={this.props.type} 
+                    placeholder={this.props.placeholder}  
+                    onChange={this.handleChange}
+                />
             </div>
         )
     }
