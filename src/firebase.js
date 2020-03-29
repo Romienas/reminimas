e0bfd9a2c1c20b23cdefe7a44d737b8efdaac0b3
Roman Juchimcuk <romasjuch@gmail.com>
2020-03-29 19:18:09 +0300

    firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "project-id.firebaseapp.com",
-    databaseURL: "https://project-id.firebaseio.com",
-    projectId: "project-id",
-    storageBucket: "project-id.appspot.com",
-    messagingSenderId: "sender-id",
-    appId: "app-id",
-    measurementId: "G-measurement-id",
+    apiKey: REACT_APP_FIREBASE_API,
+    authDomain: "reminimas-470d5.firebaseapp.com",
+    databaseURL: "https://reminimas-470d5.firebaseio.com",
+    projectId: "reminimas-470d5",
+    storageBucket: "reminimas-470d5.appspot.com",
+    messagingSenderId: "656575311576",
+    appId: "1:656575311576:web:f3f9faf8d8e7215fa731ad",
+    measurementId: "G-NC6J8X9TY5"
   };
   
 firebase.initializeApp(firebaseConfig);
