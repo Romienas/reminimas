c3b0dafe5401264ebce92935d0b8f087c6efc6db
Roman Juchimcuk <romasjuch@gmail.com>
2020-03-29 22:34:57 +0300

    fixed  onclick function bug

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 export default class Button extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        this.props.handleClick();
+    }
+
     render(){
         return(
-            <div className='button'>
+            <div className='button' onClick={this.handleClick}>
                 <div className='button__text'>
                     {this.props.buttonText}
                 </div>
