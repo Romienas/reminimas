9720ad370b3def69cb545a2c11c8b7dabde714fe
Roman Juchimcuk <romasjuch@gmail.com>
2020-03-29 22:12:50 +0300

    add function to get values from input

--- src/routes/registration.js
+++ src/routes/registration.js
@@ -3,7 +3,43 @@ import Header from '../modules/header';
 import Input from '../components/inputs/input';
 import Button from '../components/button';
 
+// let firebase = require('firebase');
+// let firebaseui = require('firebaseui')
+
+// let ui = new firebaseui.auth.AuthUI(firebase.auth());
+
 export default class Registration extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            secondPassword: ''
+        }
+
+        this.getEmail = this.getEmail.bind(this);
+        this.getPassword = this.getPassword.bind(this);
+    }
+
+    getEmail = (e) => {
+        this.setState({
+            email: e.target.value
+        });
+
+        //TODO
+        console.log(this.state.email)
+    }
+
+    getPassword = (e) => {
+        this.setState({
+            password: e.target.value
+        });
+
+        //TODO
+        console.log(this.state.password)
+    }
+
+
     render() {
         return(
             <div>
@@ -11,13 +47,13 @@ export default class Registration extends React.Component {
                 <div className='registration'>
                     <div className='registration__box'>
                         <div className='registration__input'>
-                            <Input type='email' placeholder='El. pašto adresas' />
+                            <Input type='email' placeholder='El. pašto adresas' onChange={this.getEmail} onClick={this.klik} />
                         </div>
                         <div className='registration__input'>
-                            <Input type='password' placeholder='Slaptažodis' />
+                            <Input type='password' placeholder='Slaptažodis' onChange={this.getPassword} />
                         </div>
                         <div className='registration__input'>
-                            <Input type='password' placeholder='Pakartokite slaptažodį' />
+                            <Input type='password' placeholder='Pakartokite slaptažodį' onChange={this.getSecondPassword} />
                         </div>
                         <div className='registration__button'>
                             <Button buttonText='Regisrtuotis' />
