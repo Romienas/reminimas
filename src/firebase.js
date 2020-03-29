a6f9df7a3a08f2e112f3f2bf5032621d7ef96570
Roman Juchimcuk <romasjuch@gmail.com>
2020-03-29 19:01:59 +0300

    add firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id",
+  };
+  
+firebase.initializeApp(firebaseConfig);
