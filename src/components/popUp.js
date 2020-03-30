b6915e1ff1017e5af9abc97120d417651331d04d
Roman Juchimcuk <romasjuch@gmail.com>
2020-03-30 20:54:32 +0300

    add login function

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import Input from './inputs/input';
 import Button from './button';
+import * as firebase from 'firebase';
+import InfoPop from './infoPop';
 
 export default class PopUp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            badLogin: false
+        }
+
+        this.getEmail = this.getEmail.bind(this);
+        this.getPassword = this.getPassword.bind(this);
+    }
+
+    getEmail = (email) => {
+        this.setState({
+            email
+        })
+    }
+
+    getPassword = (password) => {
+        this.setState({
+            password
+        })
+    }
+
+    signIn = () => {
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .catch((error) => {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            this.setState({
+                badLogin: true
+            })
+          });
+    }
 
     render(){
         return(
-            <div className='popup'>
-                <div className='popup__input'>
-                    <Input type='text' placeholder='Vartotojo vardas'/>
-                </div>
-                <div className='popup__input'>
-                    <Input type='password' placeholder='Slaptažodis'/>
-                </div>
-                <div className='popup__bottom'>
-                    <div className='popup__text'>
-                        <a href='/registration' >Registruotis</a>
+            <div>
+                {this.state.badLogin ? <InfoPop infoText='Neteisingai suvesti duomenys, bandykite dar kartą' /> : null}
+                <div className='popup'>
+                    <div className='popup__input'>
+                        <Input type='text' placeholder='El. paštas' changeHandler={this.getEmail} />
                     </div>
-                    <Button buttonText='Prisijungti' />
+                    <div className='popup__input'>
+                        <Input type='password' placeholder='Slaptažodis' changeHandler={this.getPassword} />
+                    </div>
+                    <div className='popup__bottom'>
+                        <div className='popup__text'>
+                            <a href='/registration' >Registruotis</a>
+                        </div>
+                        <Button buttonText='Prisijungti' handleClick={this.signIn} />
+                    </div>
                 </div>
             </div>
         )
--- src/modules/header.js
+++ src/modules/header.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import PopUp from '../components/popUp';
+import * as firebase from 'firebase';
 
 export default class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showPopUp: false
+            showPopUp: false,
+            userLogged: false
         }
+
+        this.showPopUp = this.showPopUp.bind(this);
+        this.signOut = this.signOut.bind(this);
+    }
+
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                localStorage.setItem('userEmail', user.email)
+                this.setState({
+                    userLogged: true
+                })
+            } else {
+                this.setState({
+                    userLogged: false
+                  })
+            }
+          });
     }
 
     showPopUp = () => {
         this.setState({
             showPopUp: !this.state.showPopUp
         })
+    }
+
+    signOut = () => {
+        firebase.auth().signOut().then(() => {
+            this.setState({
+                userLogged: false
+            })
+          }).catch((error) => {
+            console.log(error.code, error.message)
+          });
     }
 
     render() {
@@ -22,7 +52,13 @@ export default class Header extends React.Component {
                     <h1>Paveikslų rėminimas</h1>
                 </div>
                 <div>
-                    <div className='header__login' onClick={this.showPopUp}>Prisijungti</div>
+                    { this.state.userLogged ? 
+                        <div>
+                            <div>{localStorage.getItem('userEmail')}</div>
+                            <div onClick={this.signOut}>Atsijungti</div>
+                        </div> :
+                        <div className='header__login' onClick={this.showPopUp}>Prisijungti</div>
+                    }
                     {this.state.showPopUp === false ? null :
                     <div>
                         <div className='header__close-popup' onClick={this.showPopUp}></div>
