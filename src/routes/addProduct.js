b2e0c449d22a96a1dbbeb9e687c46a7915fcdb02
Roman Juchimcuk <romasjuch@gmail.com>
2020-04-01 22:11:29 +0300

    add input to add product page

--- src/routes/addProduct.js
+++ src/routes/addProduct.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from '../modules/header';
+import Input from '../components/inputs/input';
 
 export default class AddProduct extends React.Component {
     render() {
         return(
             <div>
                 <Header />
-                Add product
+                <div>
+                    <Input type='text' placeholder='Prekės pavadinimas' />
+                    <Input type='url' placeholder='Nuotraukos URL' />
+                    <Input type='number' placeholder='Metro kaina' />
+                    <Input type='number' placeholder='Bageto aukštis' />
+                    <Input type='number' placeholder='Bageto plotis' />
+                    
+                </div>
             </div>
         )
     }
