cfe0b19aae8599311cebd2712286a3fa422086ee
Roman Juchimcuk <romasjuch@gmail.com>
2020-04-01 20:55:04 +0300

    add admin, profile and addproduct pages

---
+++ src/routes/addProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AddProduct extends React.Component {
+    render() {
+        return(
+            <div>
+                Add product
+            </div>
+        )
+    }
+}
