89511ef884ae58869325ec26be7fb91bd029063c
Roman Juchimcuk <romasjuch@gmail.com>
2020-04-06 21:54:37 +0300

    connected to database and add loading component

--- src/routes/addProduct.js
+++ src/routes/addProduct.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import Header from '../modules/header';
 import Input from '../components/inputs/input';
-// import * as firebase from 'firebase';
+import Loading from '../components/loading'
+import * as firebase from 'firebase';
+
+let db = firebase.firestore();
 
 export default class AddProduct extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: [],
+            loaded: false
+        }
+    }
+
+    componentDidMount(){
+        db.collection('categories').doc('cat').get()
+        .then(doc => {
+            let data = doc.data();
+            data = JSON.stringify(data);
+            data = JSON.parse(data);
+
+            this.setState({
+                categories: data.category,
+                loaded: true
+            })
+        })
+    }
+
     render() {
         return(
             <div>
                 <Header />
+                {this.state.loaded ? null : <Loading />}
                 <div>
                     <Input type='text' placeholder='Prekės pavadinimas' />
                     <Input type='url' placeholder='Nuotraukos URL' />
