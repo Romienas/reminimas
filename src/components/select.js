c24d87e91e86d3fdeaf3bfb953a7c0a2dd715c88
Roman Juchimcuk <romasjuch@gmail.com>
2020-04-09 20:16:27 +0300

    Add styles and functionality

--- src/components/select.js
+++ src/components/select.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 export default class Select extends React.Component {
 
+    handleSelect = (e) => {
+        if (e.target.value !== '0'){
+            this.props.handleSelect(e.target.value)
+        }
+    }
+
     render(){
         return(
-            <div>
-                <select>
+            <div className='select'>
+                <select className='select__field' onChange={this.handleSelect}>
+                    <option value='0' defaultValue>{this.props.selectTxt}</option>
                     {this.props.selectArr.map((opt, i) => {
                         return <option key={i} value={opt}>{opt}</option>
                     })}
