6ba0c069a2e8bd4468970b2503c125476dae4053
Roman Juchimcuk <romasjuch@gmail.com>
2020-04-10 16:05:09 +0300

    add color categories feature to admin panel

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -16,14 +16,21 @@ export default class Admin extends React.Component {
             categories: [],
             catInputVal: '',
             loaded: false,
-            handleInput: false,
-            plusMinus: false
+            handleCatInput: false,
+            handleColorInput: false,
+            catPlusMinus: false,
+            colorPlusMinus: false,
+            colors: [],
+            colorInputVal:''
         }
 
         this.addCatInputVal = this.addCatInputVal.bind(this);
         this.addCategory = this.addCategory.bind(this);
-        this.handleInput = this.handleInput.bind(this);
+        this.handleCatInput = this.handleCatInput.bind(this);
         this.deleteCategory = this.deleteCategory.bind(this);
+        this.handleColorInput = this.handleColorInput.bind(this);
+        this.getColorInputValue = this.getColorInputValue.bind(this);
+        this.addColor = this.addColor.bind(this);
     }
 
     componentDidMount(){
@@ -35,6 +42,17 @@ export default class Admin extends React.Component {
 
             this.setState({
                 categories: data.category,
+                loaded: true
+            })
+        })
+        db.collection('categories').doc('colors').get()
+        .then(doc => {
+            let data = doc.data();
+            data = JSON.stringify(data);
+            data = JSON.parse(data);
+
+            this.setState({
+                colors: data.colors,
                 loaded: true
             })
         })
@@ -73,11 +91,49 @@ export default class Admin extends React.Component {
     }
 
 
-    handleInput = () => {
+    handleCatInput = () => {
         this.setState({
-            handleInput: !this.state.handleInput,
-            plusMinus: !this.state.plusMinus
+            handleCatInput: !this.state.handleCatInput,
+            catPlusMinus: !this.state.catPlusMinus
         })
+    }
+
+    handleColorInput = () => {
+        this.setState({
+            handleColorInput: !this.state.handleColorInput,
+            colorPlusMinus: !this.state.colorPlusMinus
+        })
+    }
+
+    getColorInputValue = (colorInputVal) => {
+        this.setState({
+            colorInputVal
+        })
+    }
+
+    addColor = () => {
+        let colors = this.state.colors.concat(this.state.colorInputVal);
+        this.setState({
+            colors
+        },() => {
+            db.collection('categories').doc('colors').set({
+                colors: this.state.colors
+            })
+        })
+    }
+
+    deleteColor = (color) => {
+        let index = this.state.colors.indexOf(color);
+        if( index > -1) {
+            this.state.colors.splice(index, 1);
+            this.setState({
+                colors: this.state.colors
+            },() => {
+                db.collection('categories').doc('colors').set({
+                    colors: this.state.colors
+                })
+            })
+        }
     }
 
     render() {
@@ -87,42 +143,86 @@ export default class Admin extends React.Component {
                 {this.state.loaded === false ? <Loading /> : null}
                 <div className='amdin'>
                     <div className='global__title'><h2>Valdymo pultas</h2></div>
-                    <div className='admin__prodcat'>
-                        <div className='admin__padding'>
-                            <div className='admin__prodcat-title'>
-                                <h3>Produktų kategorijos</h3>
+                    <div className='admin__container'>
+
+                        {/* HANDLE CATEGORIES */}
+                        <div className='admin__prodcat'>
+                            <div className='admin__padding'>
+                                <div className='admin__title'>
+                                    <h3>Produktų kategorijos</h3>
+                                </div>
+                                <div className='admin__categories'>
+                                    <ul>
+                                        {this.state.categories.map( (cat, i) => {
+                                            return <li key={i}>
+                                                    {cat} 
+                                                    <FontAwesomeIcon 
+                                                        className='admin__trash' 
+                                                        icon={faTrash} 
+                                                        onClick={() => this.deleteCategory(cat)}
+                                                    /> 
+                                                </li>
+                                        })}
+                                    </ul>
+                                </div>  
+                                <div className='admin__add-category' onClick={this.handleCatInput}>
+                                    <span>
+                                        <FontAwesomeIcon 
+                                            icon={this.state.catPlusMinus ? faMinus : faPlus} 
+                                        />
+                                    </span>
+                                    Pridėti kategoriją
+                                </div>
+                                { this.state.handleCatInput ?
+                                    <div>
+                                        <div className='admin__prodcat-input'>
+                                            <Input type='text' changeHandler={this.addCatInputVal} />
+                                        </div>
+                                        <div className='admin__prodcat-button'>
+                                            <Button buttonText='Pridėti' handleClick={this.addCategory} />
+                                        </div>
+                                </div> : null}
                             </div>
-                            <div className='admin__prodcat-cat'>
-                                <ul>
-                                    {this.state.categories.map( (cat, i) => {
-                                        return <li key={i}>
-                                                {cat} 
-                                                <FontAwesomeIcon 
-                                                    className='admin-prodcat-trash' 
-                                                    icon={faTrash} 
-                                                    onClick={() => this.deleteCategory(cat)}
-                                                /> 
-                                            </li>
-                                    })}
-                                </ul>
+                        </div>
+                        
+                        {/* HANDLE COLORS */}
+                        <div className='admin__prodColor'>
+                            <div className='admin__padding'>
+                                <div className='admin__title'>
+                                    <h3>Spalvos</h3>
+                                </div>
+                                <div className='admin__categories'>
+                                    <ul>
+                                        {this.state.colors.map((color, i) => {
+                                            return <li key={i}>
+                                                        {color}
+                                                        <FontAwesomeIcon 
+                                                            className='admin__trash' 
+                                                            icon={faTrash} 
+                                                            onClick={() => this.deleteColor(color)}
+                                                        />     
+                                                    </li>
+                                        })}
+                                    </ul>
+                                </div>
+                                <div className='admin__add-category' onClick={this.handleColorInput}>
+                                    <span>
+                                        <FontAwesomeIcon 
+                                            icon={this.state.colorPlusMinus ? faMinus : faPlus} 
+                                        />
+                                    </span>
+                                    Pridėti spalvą
+                                </div>
+                                { this.state.handleColorInput ?
+                                    <div>
+                                        <div className='admin__prodcat-input'>
+                                            <Input type='text' changeHandler={this.getColorInputValue} />
+                                        </div>
+                                        <div className='admin__prodcat-button'>
+                                            <Button buttonText='Pridėti' handleClick={this.addColor} />
+                                        </div>
+                                </div> : null}
                             </div>
-                            <div className='admin__prodcat-add' onClick={this.handleInput}>
-                                <span>
-                                    <FontAwesomeIcon 
-                                        icon={this.state.plusMinus ? faMinus : faPlus} 
-                                    />
-                                </span>
-                                Pridėti kategoriją
-                            </div>
-                            { this.state.handleInput ?
-                                <div>
-                                    <div className='admin__prodcat-input'>
-                                        <Input type='text' changeHandler={this.addCatInputVal} />
-                                    </div>
-                                    <div className='admin__prodcat-button'>
-                                        <Button buttonText='Pridėti' handleClick={this.addCategory} />
-                                    </div>
-                            </div> : null}
                         </div>
                     </div>
                 </div>
