f64a20cecdd0418c6baf03e7758eb233660811e6
Roman Juchimcuk <romasjuch@gmail.com>
2020-04-26 17:27:43 +0300

    add image gallery to product order window

--- src/modules/orderProduct.js
+++ src/modules/orderProduct.js
@@ -17,10 +17,21 @@ export default class OrderProduct extends React.Component {
                 document.getElementById(img).src = url
             })
         });
+
+        // Set gallery main image
+        storageRef.child(`images/${this.props.productObj.image[0]}`)
+        .getDownloadURL()
+        .then(url => {
+            document.getElementById('orderProduct__mainImg').src = url
+        })
     }
 
     handleClick = () =>{
         this.props.handleClick('')
+    }
+    
+    changeImage = (e) => {
+        document.getElementById('orderProduct__mainImg').src = e.target.src
     }
 
     render() {
@@ -37,18 +48,28 @@ export default class OrderProduct extends React.Component {
                         <div className='global__title'>
                             <h2>Rėmo užsakymas</h2>    
                         </div>
-                        <div className='orderProduct__header'>
-                            {this.props.productObj.image.map((img, i) => {
-                                return <img id={img} key={i} src='' alt={this.props.productObj.productName} />
-                            })}
-                            <div>
-                                <h3>{this.props.productObj.productName}</h3>
+                        <div>
+                            <h3>{this.props.productObj.productName}</h3>
+                        </div>
+                        <div className='orderProduct__gallery'>
+                            <div className='orderProduct__gallery-mainImg'>
+                                <img id='orderProduct__mainImg' src='' alt={this.props.productObj.productName} />
                             </div>
+                            <div className='orderProduct__gallery-images'>
+                                {this.props.productObj.image.map((img, i) => {
+                                    return <img 
+                                                id={img} 
+                                                key={i} 
+                                                src='' 
+                                                alt={this.props.productObj.productName} 
+                                                onClick={this.changeImage}
+                                            />
+                                })}
+                            </div>
+                        </div>
+                        <div className='orderProduct__header'>                          
                             <div>
                                 Kaina: {this.props.productObj.price} &euro;/m
-                            </div>
-                            <div onClick={this.handleClick}>
-                                uzdaryti
                             </div>
                         </div>
                     </div>
