b894264688ab8c76e8573db7d6fa13018353b76f
Roman Juchimcuk <romasjuch@gmail.com>
2020-05-20 08:35:21 +0300

    add orders module to admin panel

---
+++ src/modules/adminOrders.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class AdminOrders extends React.Component {
+    render(){
+        return(
+            <div>
+                <div className='admin__title'>
+                    <h3>Užsakymų valdymas</h3>
+                </div>
+            </div>
+        )
+    }
+}
