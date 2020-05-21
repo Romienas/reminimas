373205c00604f9aa4971a50e096e434844b6b767
Roman Juchimcuk <romasjuch@gmail.com>
2020-05-21 08:27:03 +0300

    get data from database and sort by time

--- src/modules/adminOrders.js
+++ src/modules/adminOrders.js
@@ -1,6 +1,36 @@
 import React from 'react'
+import * as firebase from 'firebase'
+
+let db = firebase.firestore()
 
 export default class AdminOrders extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ordersArr: []
+        }
+    }
+
+    componentDidMount() {
+        db.collection('orders')
+        .get()
+        .then(snap => {
+            let ordersArr = []
+            snap.forEach(doc => {
+                let data = doc.data()
+                data = JSON.stringify(data)
+                data = JSON.parse(data)
+                ordersArr.push(data)
+            })
+            ordersArr.sort((c, d) => {
+                let a = new Date(c.date)
+                let b = new Date(d.date)
+                return b - a
+            })
+            console.log(ordersArr)
+        })
+    }
+
     render(){
         return(
             <div>
