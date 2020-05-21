d736ed5edbcb6453345367abd46cb1e42855778d
Roman Juchimcuk <romasjuch@gmail.com>
2020-05-21 08:44:33 +0300

    add orders list to admin panel

--- src/modules/adminOrders.js
+++ src/modules/adminOrders.js
@@ -27,6 +27,9 @@ export default class AdminOrders extends React.Component {
                 let b = new Date(d.date)
                 return b - a
             })
+            this.setState({
+                ordersArr
+            })
             console.log(ordersArr)
         })
     }
@@ -36,6 +39,28 @@ export default class AdminOrders extends React.Component {
             <div>
                 <div className='admin__title'>
                     <h3>Užsakymų valdymas</h3>
+                </div>
+                <div className='adminOrders__list'>
+                    <div>
+                        {this.state.ordersArr.map((item, i) => {
+                            return (
+                                <div key={i} className='adminOrders__list-item'>
+                                    <div>
+                                        {item.order_id}
+                                    </div>
+                                    <div>
+                                        {item.date}
+                                    </div>
+                                    <div>
+                                        {item.product}
+                                    </div>
+                                    <div>
+                                    Kaina: {item.price} &euro;
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         )
